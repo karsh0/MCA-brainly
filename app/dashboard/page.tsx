9c1ddcbff30c0af97b4326911a8cc2043dc0960d
Karan <karansharma192004@gmail.com>
2025-01-30 15:15:49 +0530

    UPDATE: Fixed again dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -30,7 +30,7 @@ export default function DashboardPage() {
 
   // Fetch session data on component mount
   useEffect(() => {
-    axios.get<Session>('http://localhost:3000/api/dashboard').then((response) => setSession(response.data))
+    axios.get<Session>('/api/dashboard').then((response) => setSession(response.data))
   }, []);
 
   const handleShare = async () => {
